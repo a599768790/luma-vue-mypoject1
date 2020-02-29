21f79aeca757e47b6a9465c7e9606d6ab2eb6d8d
Fe_lyh <599768790@qq.com>
2020-02-29 14:58:07 +0800

    zz

---
+++ src/icons/SvgIcon.vue
@@ -0,0 +1,61 @@
+<template>
+  <svg :class="svgClass" aria-hidden="true">
+    <use :xlink:href="iconName"></use>
+  </svg>
+</template>
+<script>
+import { ref, computed, onMounted } from "@vue/composition-api";
+export default {
+  name: "svgIcon",
+  // props: ['iconClass', 'className'],
+  props: {
+    // 分页
+    iconClass: {
+      type: String, // 定义接收的值类型
+      default: ""
+    },
+    className: {
+      type: String,
+      default: ""
+    }
+  },
+  setup(props) {
+    console.log(props);
+    const msg = ref("手把手撸码的说法的前端");
+    const number = ref(1);
+
+    // iconClass传进来计算返回一个iconName 计算属性，得出最终结果，并返回
+    const iconName = computed(() => `#icon-${props.iconClass}`);
+
+    // 监听组件className有传值进来就 => 计算属性，得出最终结果，并返回
+    const svgClass = computed(() => {
+      if (props.className) {
+        return `svg-icon ${props.className}`;
+      } else {
+        return `svg-icon`;
+      }
+    });
+    // const price = computed(() => {
+    //   return number.value *10
+
+    // });
+    // const aa = () =>{
+    //   number.value = 2
+    //   console.log(price.value)
+    // }
+
+    return {
+      msg,
+      iconName,
+      svgClass
+    };
+  }
+};
+</script>
+<style lang="scss" scoped>
+.svg-icon {
+  width: 1em;
+  height: 1em;
+  fill: currentColor;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+// import Home from "./views/Home.vue";
 
 Vue.use(Router);
 
@@ -8,8 +8,9 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "home",
-      component: Home
+      redirect: "Login"
+      // name: "home",
+      // component: Home
     },
     {
       path: "/about",
@@ -18,7 +19,12 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+        import(/* webpackChunkName: "about" */ "@/views/About.vue")
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: () => import("@/views/Login/Index.vue")
     }
   ]
 });
